663cf99fb6c294a10dcfbf6129363bba23e62829
Daniela Marcela Becerra Sosa <90697179+DanielaBecerra1@users.noreply.github.com>
2024-07-01 21:45:48 -0500

    Delete StreamFusion directory

--- StreamFusion/buscador-service/src/main/java/com/StreamFusion/buscador_service/error/PlatformNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.StreamFusion.buscador_service.error;
-
-public class PlatformNotFoundException extends Exception {
-    public PlatformNotFoundException(String message) {
-        super(message);
-    }
-}
